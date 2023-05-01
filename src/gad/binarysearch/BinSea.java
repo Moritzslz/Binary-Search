efc6b005a6e8ece269bf25d626b0894fc8d9ef73
moritzslz <schultzmoritz@gmail.com>
2023-05-01 21:39:49 +0200

    Method one changes

--- src/gad/binarysearch/BinSea.java
+++ src/gad/binarysearch/BinSea.java
@@ -8,90 +8,95 @@ public final class BinSea {
     }
 
     public static int search(int[] sortedData, int value, Result result) {
+        int[] indices = binarySearch(sortedData, value, result);
+        int mid = indices[0];
+        int lowerBound = indices[1];
+        int upperBound = indices[2];
+
+        if(mid != -1)
+            return mid;
+        else if (lowerBound != -1)
+            return lowerBound;
+        else
+            return upperBound;
+    }
+
+    public static int[] binarySearch(int[] sortedData, int value, Result result) {
         int lowerBound = 0;
         int upperBound = sortedData.length - 1;
         int mid = (lowerBound + upperBound) / 2;
+        int[] indices = new int[3];
+        indices[0] = mid;
+        indices[1] = lowerBound;
+        indices[2] = upperBound;
 
         //Edge case: all values are the same
-        if (sortedData[lowerBound] == sortedData[upperBound])
-            return mid;
+        if (sortedData[indices[1]] == sortedData[indices[2]])
+            return indices;
 
         //The maximum amount of iterations is half of the array length
         //That is the case when the searched index is at the most outer
         //edge of the array you get when splitting the given array in half.
         for (int i = 0; i < sortedData.length / 2; i++) {
-            mid = (lowerBound + upperBound) / 2;
+            indices[0] = (indices[1] + indices[2]) / 2;
 
             //Termination Condition
-            if (lowerBound == upperBound) {
-                if (sortedData[upperBound] == value)
-                    result.addStep(upperBound);
-                return upperBound;
-            } else if (lowerBound == upperBound - 1) {
-                    if (sortedData[lowerBound] == value) {
-                        result.addStep(lowerBound);
-                        return lowerBound;
-                    } else if (sortedData[upperBound] == value) {
-                        result.addStep(upperBound);
-                        return upperBound;
-                    } else if (sortedData[lowerBound] < value && value < sortedData[upperBound]) {
-                        //If the searched for value lies between lower bound and upper bound then the searched for value
-                        //is not contained in the array
-                        //-> return upperBound (next bigger value)
-                        result.addStep(upperBound);
-                        return upperBound;
-                    } else if (sortedData[lowerBound] > value) {
-                        //If the lower bound is 0, and it's value is bigger than the searched for value then the searched
-                        //for value is not contained in the array
-                        //-> return lowerBound (next bigger value)
-                        result.addStep(lowerBound);
-                        return lowerBound;
-                    } else if (sortedData[upperBound] < value) {
-                        //If the upperBound bound is sortedDate.length - 1, and it's value is smaller than the searched
-                        //for value then the searched for value is not contained in the array
-                        //-> return upperBound (next smaller value)
-                        result.addStep(upperBound);
-                        return upperBound;
-                    }
+            if (indices[1] == indices[2]) {
+                if (sortedData[indices[2]] == value)
+                    result.addStep(indices[2]);
+                return indices;
+            } else if (indices[1] == indices[2] - 1) {
+                if (sortedData[indices[1]] == value) {
+                    result.addStep(indices[1]);
+                    indices[0] = -1;
+                    indices[2] = -1;
+                    return indices;
+                } else if (sortedData[indices[2]] == value) {
+                    result.addStep(indices[2]);
+                    indices[0] = -1;
+                    indices[1] = -1;
+                    return indices;
+                } else if (sortedData[indices[1]] < value && value < sortedData[indices[2]]) {
+                    //If the searched for value lies between lower bound and upper bound then the searched for value
+                    //is not contained in the array
+                    //-> return indices[2] (next bigger value)
+                    result.addStep(indices[2]);
+                    indices[0] = -1;
+                    return indices;
+                } else if (sortedData[indices[1]] > value) {
+                    //If the lower bound is 0, and it's value is bigger than the searched for value then the searched
+                    //for value is not contained in the array
+                    //-> return indices[1] (next bigger value)
+                    result.addStep(indices[1]);
+                    indices[2] = indices[1];
+                    indices[0] = -1;
+                    return indices;
+                } else if (sortedData[indices[2]] < value) {
+                    //If the indices[2] bound is sortedDate.length - 1, and it's value is smaller than the searched
+                    //for value then the searched for value is not contained in the array
+                    //-> return indices[2] (next smaller value)
+                    result.addStep(indices[2]);
+                    indices[0] = -1;
+                    indices[1] = indices[2];
+                    return indices;
                 }
+            }
 
-            result.addStep(mid);
+            result.addStep(indices[0]);
 
-            if (sortedData[mid] > value) {
-                upperBound = mid - 1;
-            } else if (sortedData[mid] < value) {
-                lowerBound = mid + 1;
-            } else if (sortedData[mid] == value) {
+            if (sortedData[indices[0]] > value) {
+                indices[2] = indices[0] - 1;
+            } else if (sortedData[indices[0]] < value) {
+                indices[1] = indices[0] + 1;
+            } else if (sortedData[indices[0]] == value) {
                 break;
             }
         }
-        return mid;
+        return indices;
     }
 
     public static int search(int[] sortedData, int value, boolean lowerBound, Result result) {
-        int idx = search(sortedData, value, result);
-        int maxLen = sortedData.length - 1;
-
-        if (!lowerBound) {
-            if (idx == maxLen) {
-                if (sortedData[idx] > value)
-                    return -1;
-            }
-            while (idx < maxLen && sortedData[idx] < value)
-                idx++;
-            while (idx < maxLen && sortedData[idx+1] == value)
-                idx++;
-        } else {
-            if (idx == 0) {
-                if (sortedData[idx] < value)
-                    return -1;
-            }
-            while (idx < 0 && sortedData[idx] > value)
-                idx--;
-            while (idx < 0 && sortedData[idx-1] == value)
-                idx--;
-        }
-        return idx;
+        return 0;
     }
 
     public static Interval search(int[] sortedData, NonEmptyInterval valueRange, Result resultLower, Result resultHigher) {
@@ -104,8 +109,8 @@ public final class BinSea {
         int[] array3 = new int[] { 2, 4, 8, 16, 32, 64, 128, 256 }; //7
         int[] array4 = new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2 }; //8
 
-        System.out.println(search(array2, 7, new StudentResult()));
-        System.out.println(search(array2, 100, new StudentResult()));
+        System.out.println(search(array, 7, new StudentResult()));
+        System.out.println(search(array, 1500, new StudentResult()));
 
         System.out.println(search(array, 1, false, new StudentResult()));
         System.out.println(search(array, 100, true, new StudentResult()));
