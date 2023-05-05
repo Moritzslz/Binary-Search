78502c2343fc750b252cf0d7c649bdadb8a98b80
moritzslz <schultzmoritz@gmail.com>
2023-05-05 13:44:04 +0200

    Method three VII

--- src/gad/binarysearch/BinSea.java
+++ src/gad/binarysearch/BinSea.java
@@ -62,6 +62,12 @@ public final class BinSea {
     }
 
     public static Interval search(int[] sortedData, NonEmptyInterval valueRage, Result resultLower, Result resultHigher) {
+
+        if (valueRage.getFrom() > sortedData[sortedData.length - 1])
+            return Interval.EmptyInterval.getEmptyInterval();
+        if(valueRage.getTo() < sortedData[0])
+            return Interval.EmptyInterval.getEmptyInterval();
+
         int lowerIntervalBound = search(sortedData, valueRage.getFrom(), true, resultLower);
         if (lowerIntervalBound == -1)
             return Interval.EmptyInterval.getEmptyInterval();
